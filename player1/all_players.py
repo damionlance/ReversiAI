0882a1e41229f9a8e585dd82826d7eb010ba46da
Sean McQuillan <mcquill99@gmail.com>
2019-10-22 15:20:05 -0400

    Added combined features to all_players.py - Erich

--- player1/all_players.py
+++ player1/all_players.py
@@ -1,42 +1,43 @@
 from player1.MiniMaxPlayer import MiniMaxComputerPlayer, simple_evaluate, difference_heuristic, combined_heuristics
+from player1.damionWork.BeamSearch import beam_search
 
 
-def get_default_player(symbol):
+def get_default_player(symbol, depth):
     """
     :returns: a default minimax player that can operate successfully on a given 8x8 board
     """
 
-    return MiniMaxComputerPlayer(symbol, 3, difference_heuristic, pruning=False)
+    return MiniMaxComputerPlayer(symbol, depth, difference_heuristic, pruning=False)
 
 
-def get_player_pruning(symbol):
+def get_player_pruning(symbol, depth):
     """
     :Tim Clerico:
     :Alpha Beta Pruning:
     :returns: an enhanced minimax player that can operate successfully on a given 8x8 board
     """
-    return MiniMaxComputerPlayer(symbol, 3, difference_heuristic, pruning=True)
+    return MiniMaxComputerPlayer(symbol, depth, difference_heuristic, pruning=True)
 
 
-def get_player_heuristic(symbol):
+def get_player_heuristic(symbol, depth):
     """
     :Sean McQuilan:
     :Better Heuristics:
     :returns: an enhanced minimax player that can operate successfully on a given 8x8 board
     """
-    return MiniMaxComputerPlayer(symbol, 3, combined_heuristics, pruning=False)
+    return MiniMaxComputerPlayer(symbol, depth, combined_heuristics, pruning=False)
 
 
-def get_player_beam(symbol):
+def get_player_beam(symbol, depth):
     """
     :Damion Lance:
     :Beam Search:
     :returns: an enhanced minimax player that can operate successfully on a given 8x8 board
     """
-    pass
+    return MiniMaxComputerPlayer(symbol, depth, combined_heuristics, pruning=False, beam_search=beam_search)
 
 
-def get_player_d(symbol):
+def get_player_d(symbol,depth):
     """
     :author:
     :enchancement:
@@ -45,8 +46,8 @@ def get_player_d(symbol):
     pass
 
 
-def get_combined_player(symbol):
+def get_combined_player(symbol, depth):
     """
     :returns: the best combination of the minimax enhancements that your team can create
     """
-    return MiniMaxComputerPlayer(symbol, 3, combined_heuristics, pruning=True)
+    return MiniMaxComputerPlayer(symbol, depth, combined_heuristics, pruning=True, beam_search=beam_search)
